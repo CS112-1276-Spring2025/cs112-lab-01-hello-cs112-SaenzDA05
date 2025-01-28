4f7afb981029db485e9cf6a8ae8e51a07e6c45f9
SaenzDA05 <shaun.saenz@icloud.com>
2025-01-28 21:12:37 +0000

    part 2

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to CS112!");
+        Main.printaboutMe();
+
     }
-}
+    
+
+    public static void printaboutMe() {
+        System.out.println(
+                "I like snowboarding, sleep, videogames, Light novels, anime, and working out");
+        boolean NeedSleep = Main.MySleep(0);
+        System.out.println("Did you get enought sleep "+ (NeedSleep ? "Yes" : "No")); 
+
+    }
+
+    public static boolean MySleep(int topic) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many hours of sleep do you get a night");
+        int sleepHours = scanner.nextInt();
+        if (sleepHours < 9) {
+            return false;
+    }
+        else
+        {
+            return true;
+        }
+        }
+    }
+    
